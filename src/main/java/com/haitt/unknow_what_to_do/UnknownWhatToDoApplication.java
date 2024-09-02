37b529cae93c5bd1cfb514adb906e289f603a19d
Jason1420 <hai.truong1420@gmail.com>
2024-09-02 09:57:38 +0700

    Init project

---
+++ src/main/java/com/haitt/unknow_what_to_do/UnknownWhatToDoApplication.java
@@ -0,0 +1,13 @@
+package com.haitt.unknow_what_to_do;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnknownWhatToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnknownWhatToDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haitt/unknow_what_to_do/UnknowWhatToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haitt.unknow_what_to_do;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnknownWhatToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
